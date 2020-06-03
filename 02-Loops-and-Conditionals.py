47b820a732aae725735bbe55cf4864d4d034cba8
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-03 15:31:22 +0100

    add punctuation and capitalization

--- 02-Loops-and-Conditionals.py
+++ 02-Loops-and-Conditionals.py
@@ -1,13 +1,21 @@
+def sentence_maker(string):
+  if string.startswith('how') or string.startswith('who') or string.startswith('what') or string.startswith('why') or string.startswith('when') or string.startswith('where'):
+    return string.capitalize() + '?'
+  else:
+    return string.capitalize() + '.'
+
+# print(sentence_maker(input("Say something:")))
 
 userList = []
 
 while True:
   user_words = input("Say something:")
+  new_words = sentence_maker(user_words)
   if user_words == "/end":
     print(" ".join(map(str, userList)))
     break
   else:
-    userList.append(user_words)
+    userList.append(new_words)
 
 
 
