3ade64c29ac44dbe438a93f9c514e3efb867a999
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-10 11:34:35 +0100

    add elif to allow definitions for capitalized proper nouns and acronyms

--- 04-Dictionary/dictionary.py
+++ 04-Dictionary/dictionary.py
@@ -8,14 +8,27 @@ def definition(word):
   word = word.lower()
   if word in data:
     return data[word]
+  elif word.title() in data:
+    return data[word.title()]
+  elif word.upper() in data:
+    return data[word.upper()]
   elif len(get_close_matches(word, data.keys())) > 0:
-    answer = input("Did you mean %s instead? Type (y) for yes or (n) for no." % get_close_matches(word, data.keys())[0])
+    answer = input("Did you mean %s instead? Type (y) for yes or (n) for no: " % get_close_matches(word, data.keys())[0])
     if answer == "y":
-      return "woop"
+      return data[get_close_matches(word, data.keys())[0]]
+    elif answer == "n":
+      return "Sorry, this word does not exist, please check it and try again."
     else:
-      return "oh no"
+      return "Sorry, this word does not exist, please check it and try again."
   else:
     return "This word is not in our dictionary, please check your spelling and try again"
 
 word = input("Enter word:")
-print(definition(word))
+
+output = definition(word)
+
+if type(output) == list:
+  for item in output:
+    print(item)
+else:
+  print(output)
