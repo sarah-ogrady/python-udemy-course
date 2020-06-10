55081fc589050d006143ee8c171451abc794c373
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-10 15:22:14 +0100

    created query in sql for dictionary

---
+++ 03-Dictionary-with-sql/dictionary_sql.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+user = "ardit700_student",
+password = "ardit700_student",
+host = "108.167.140.122",
+database = "ardit700_pm1database"
+)
+
+
+cursor = con.cursor()
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = 'inlay'")
+
+results = cursor.fetchall()
+
+print(results[0])
