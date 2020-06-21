284973783324f599fa0a606d5e7940ccff77470d
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-21 23:49:44 +0100

    add list and loop

--- 06-Web-Map/map1.py
+++ 06-Web-Map/map1.py
@@ -3,7 +3,7 @@ map = folium.Map(location=[38.2, -99.1], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1], [32.2, -97.1]]:
+for coordinates in [[38.2, -99.1], [32.2, -97.1], [36.2, -91.1], [37.2, -95.1]]:
     fg.add_child(folium.Marker(location=coordinates, popup="hi im a marker", icon=folium.Icon('green')))
 
 map.add_child(fg)
