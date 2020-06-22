3614664bf8f1920174a2dd2cca39c5fe08035b68
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-22 12:22:42 +0100

    add a tag with link for volcano wiki page

--- 06-Web-Map/map1.py
+++ 06-Web-Map/map1.py
@@ -8,7 +8,7 @@ name = list(data["NAME"])
 elev = list(data["ELEV"])
 
 html = """<h4>Volcano information:</h4>
-<p>Name: %s</p>
+Name: <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a>
 <p>Height: %s m</p>
 """
 
@@ -17,7 +17,7 @@ map = folium.Map(location=[38.2, -99.1], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el, nm in  zip(lat, lon, elev, name):
-    iframe = folium.IFrame(html=html % (nm, el), width=200, height=100)
+    iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
     fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon('blue')))
 
 map.add_child(fg)
