2d3d9214d747ea86124edfe5eabb6913cc74a7d8
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-22 11:35:36 +0100

    add loop over two lists of lon and lat to create 62 markers

--- 06-Web-Map/map1.py
+++ 06-Web-Map/map1.py
@@ -1,10 +1,18 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+
+lat = list(data["LAT"])
+
+lon = list(data["LON"])
+
 map = folium.Map(location=[38.2, -99.1], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1], [32.2, -97.1], [36.2, -91.1], [37.2, -95.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="hi im a marker", icon=folium.Icon('green')))
+for lat, lon in  zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lat, lon], popup="hi im a marker", icon=folium.Icon('green')))
 
 map.add_child(fg)
 map.save("Map1.html")
