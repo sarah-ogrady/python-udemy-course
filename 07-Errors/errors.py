4cb0ee9aeac0c37abdbff94f5afdfef0788fbeb3
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-22 15:51:16 +0100

    add error notes

---
+++ 07-Errors/errors.py
@@ -0,0 +1,12 @@
+a = 1
+b = "2"
+print(int(2.5)
+print(a + b)
+
+# ERROR INFO
+# There are syntax errors and exception errors
+# The above line will throw an error in line 4 and not line 3 even
+# though the missing ) is on line 3
+# line four the "+" "does not support one of the two: int or str "
+
+
