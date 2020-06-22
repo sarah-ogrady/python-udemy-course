eb785fc4d8d576858167c7e6683b4b716fae060e
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-22 12:15:08 +0100

    add html with interpolated name and elevation, loop now iterates four separate lists

--- 06-Web-Map/map1.py
+++ 06-Web-Map/map1.py
@@ -2,17 +2,23 @@ import folium
 import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
-
 lat = list(data["LAT"])
-
 lon = list(data["LON"])
+name = list(data["NAME"])
+elev = list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+<p>Name: %s</p>
+<p>Height: %s m</p>
+"""
 
 map = folium.Map(location=[38.2, -99.1], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lat, lon in  zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lat, lon], popup="hi im a marker", icon=folium.Icon('green')))
+for lt, ln, el, nm in  zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (nm, el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon('blue')))
 
 map.add_child(fg)
 map.save("Map1.html")
