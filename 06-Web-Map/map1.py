2a3f821d0c36405adfa343452e318ac318048341
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-22 14:58:56 +0100

    add choropleth layering

--- 06-Web-Map/map1.py
+++ 06-Web-Map/map1.py
@@ -32,6 +32,9 @@ for lt, ln, el, nm in  zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
     fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=what_color(el), icon='fa-tree', prefix='fa')))
 
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x["properties"]["POP2005"] < 20000000 else 'red'}))
+
 map.add_child(fg)
 map.save("Map1.html")
 
