3a2f5804649e085eec3b48c3dc11b0bf83cf2303
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-22 15:23:06 +0100

    refactor to have two feature groups to be able to switch layers on and off

--- 06-Web-Map/map1.py
+++ 06-Web-Map/map1.py
@@ -15,7 +15,7 @@ Name: <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a
 
 map = folium.Map(location=[38.2, -99.1], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 def what_color(elevation):
   if elevation <= 1000:
@@ -30,11 +30,14 @@ def what_color(elevation):
 
 for lt, ln, el, nm in  zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=what_color(el), icon='fa-tree', prefix='fa')))
+    fgv.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=what_color(el), icon='fa-tree', prefix='fa')))
 
+fgp = folium.FeatureGroup(name="Population")
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x["properties"]["POP2005"] < 20000000 else 'red'}))
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x["properties"]["POP2005"] < 20000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
 
