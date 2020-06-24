4cded4de4e38b3e646edf236326eb1529941a101
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-24 22:05:51 +0100

    add new route 'about'

--- 08-Website-with-Flask/script1.py
+++ 08-Website-with-Flask/script1.py
@@ -1,1 +1,14 @@
 from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Homepage here'
+
+@app.route('/about')
+def about():
+    return 'Website content here'
+
+if __name__=="__main__":
+    app.run(debug=True)
