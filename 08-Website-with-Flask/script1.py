2f9bbf00b7102a1cd6a5aa4b91e312d9edeef306
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-25 09:53:24 +0100

    add render_template to then render an html file

--- 08-Website-with-Flask/script1.py
+++ 08-Website-with-Flask/script1.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Homepage here'
+    return render_template("home.html")
 
 @app.route('/about')
 def about():
