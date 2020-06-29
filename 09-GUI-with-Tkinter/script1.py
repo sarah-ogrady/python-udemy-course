6bc5744c7ac0139cd0ff6dd1e8daca020f024574
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-29 14:48:23 +0100

    add my first few widgets :)

--- 09-GUI-with-Tkinter/script1.py
+++ 09-GUI-with-Tkinter/script1.py
@@ -3,6 +3,12 @@ from tkinter import *
 window=Tk()
 
 b1=Button(window, text="Execute")
-b1.pack()
+b1.grid(row=0, column=0)
+
+e1=Entry(window)
+e1.grid(row=0, column=1)
+
+t1=Text(window, height=1, width=20)
+t1.grid(row=0, column=2)
 
 window.mainloop()
