b0effe8e0a3d849e838c93cb375ea40a8a633b56
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-29 15:18:27 +0100

    add .get() command to enable use of typed value

--- 09-GUI-with-Tkinter/script1.py
+++ 09-GUI-with-Tkinter/script1.py
@@ -2,10 +2,15 @@ from tkinter import *
 
 window=Tk()
 
-b1=Button(window, text="Execute")
+def km_to_miles():
+  print(e1_value.get())
+  t1.insert(END, e1_value.get())
+
+b1=Button(window, text="Execute", command=km_to_miles)
 b1.grid(row=0, column=0)
 
-e1=Entry(window)
+e1_value=StringVar()
+e1=Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
 t1=Text(window, height=1, width=20)
