75e867a150c2ce4ac20c38bb2b7e0e754c9fb9ba
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-06-29 17:02:55 +0100

    add view to see what's now in database

--- 10-Interacting-with-Databases/script1.py
+++ 10-Interacting-with-Databases/script1.py
@@ -1,1 +1,25 @@
 import sqlite3
+
+def create_table():
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+def insert(item,quantity,price):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?, ?, ?)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM store ")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+print(view())
