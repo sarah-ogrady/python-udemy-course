3ba5d68c829419057e23e2c838ddb9d3339a7c10
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-07-06 16:23:32 +0100

    add search function

--- 11-Database-App/backend.py
+++ 11-Database-App/backend.py
@@ -1,17 +1,37 @@
 import sqlite3
 
 def connect():
-    conn=sqlite2.connect("books.db")
+    conn=sqlite3.connect("books.db")
     cur=conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER) ")
     conn.commit()
     conn.close()
 
-    connect()
 
-def insert():
+def insert(title,author,year,isbn):
     conn=sqlite3.connect("books.db")
     cur=conn.cursor()
-    cur.execute("")
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title,author,year,isbn))
     conn.commit()
     conn.close()
+
+def view():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title='',author='',year='',isbn=''):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+connect()
+insert("The Sun and the sky","Someone",1934,8973459457)
+print(view())
+print(search(author='Someone'))
