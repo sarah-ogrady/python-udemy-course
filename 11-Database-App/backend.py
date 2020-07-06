e7f0ee4df2468748f2a44658bafa7c2bbd5eb207
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-07-06 12:27:14 +0100

    add insert function

--- 11-Database-App/backend.py
+++ 11-Database-App/backend.py
@@ -6,3 +6,12 @@ def connect():
     cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER) ")
     conn.commit()
     conn.close()
+
+    connect()
+
+def insert():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("")
+    conn.commit()
+    conn.close()
