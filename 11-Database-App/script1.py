d03fd443e765aded9abb6afe51fbde32d2d44607
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-07-06 12:06:54 +0100

    set width of buttons

--- 11-Database-App/script1.py
+++ 11-Database-App/script1.py
@@ -54,22 +54,22 @@ sb1.grid(row=2,column=2,rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1=Button(window,text="View all")
+b1=Button(window,text="View all",width=12)
 b1.grid(row=2,column=3)
 
-b2=Button(window,text="Search entry")
+b2=Button(window,text="Search entry",width=12)
 b2.grid(row=3,column=3)
 
-b3=Button(window,text="Add entry")
+b3=Button(window,text="Add entry",width=12)
 b3.grid(row=4,column=3)
 
-b4=Button(window,text="Update")
+b4=Button(window,text="Update",width=12)
 b4.grid(row=5,column=3)
 
-b5=Button(window,text="Delete")
+b5=Button(window,text="Delete",width=12)
 b5.grid(row=6,column=3)
 
-b6=Button(window,text="Close")
+b6=Button(window,text="Close",width=12)
 b6.grid(row=7,column=3)
 
 window.mainloop()
