db1eca9dee44618713856086b3f883c486ea324a
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-07-06 11:29:37 +0100

    files for app 4

---
+++ 11-Database-App/script1.py
@@ -0,0 +1,16 @@
+"""
+A programme that stores information:
+Title, Author
+Year, ISBN
+
+User can:
+
+View all records
+Search an entry
+Add an entry
+Update entry
+Delete
+Close
+"""
+
+from tkinter import *
